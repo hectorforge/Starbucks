2fb0558b5be8ff579fbe16d2e0bb383d840fb7cc
hectorforge <hector.hernandez.dev@gmail.com>
2025-06-23 20:14:33 -0500

    Ayudando

---
+++ demo/src/main/java/com/starbuks/app/controller/VentaController.java
@@ -0,0 +1,11 @@
+package com.starbuks.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ventas")
+@RequiredArgsConstructor
+public class VentaController {
+}
