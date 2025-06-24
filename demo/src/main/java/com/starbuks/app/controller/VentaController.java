87535df12aebc8f43b9603e5b1c1f1f920b66314
hectorforge <hector.hernandez.dev@gmail.com>
2025-06-23 20:15:37 -0500

    Estructura base para tu contrlador venta

--- demo/src/main/java/com/starbuks/app/controller/VentaController.java
+++ demo/src/main/java/com/starbuks/app/controller/VentaController.java
@@ -1,11 +1,22 @@
 package com.starbuks.app.controller;
 
+import com.starbuks.app.entitys.bean.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/ventas")
 @RequiredArgsConstructor
 public class VentaController {
+
+    // LISTAR TODOS LAS VENTAS
+    @GetMapping
+    public String listarUsuarios(Model model) {
+        return "ventas/listar";
+    }
 }
