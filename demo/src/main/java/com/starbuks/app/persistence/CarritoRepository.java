85e7c0dbef74dbf58eea21f94cf8b6e00d73f58e
Facundo Flores <miiguelflores1701@gmail.com>
2025-06-28 19:56:35 -0500

    quitando clases sin uso

--- demo/src/main/java/com/starbuks/app/persistence/CarritoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.starbuks.app.persistence;
-
-import com.starbuks.app.entitys.bean.Carrito;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface CarritoRepository extends JpaRepository<Carrito, Long> {
-    Optional<Carrito> findByUsuarioId(Long usuarioId);
-}
